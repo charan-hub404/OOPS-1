e34f330531ffec08af807cfab92e4c76364f6efd
cherry <sreecharanlingampalli@gmail.com>
2025-08-02 10:53:29 +0530

    Add files via upload

---
+++ Fact.java
@@ -0,0 +1,16 @@
+package oops1;
+
+public class Fact
+{
+ public static void main(String[] args)
+ {
+ int fact=1,x = 5,i;
+ for(i=1;i<=x;i++)
+{
+fact *= i;
+
+} 
+ 
+System.out.printf("Factorial of 5=%d", fact);
+}
+}
---
+++ Firstwhile.java
@@ -0,0 +1,16 @@
+package oops1;
+
+public class Firstwhile
+{
+public static void main(String[] args) 
+ {
+ int fact = 1;
+ int x = 5;
+ while(x>=1) 
+  {
+ fact = fact * x; 
+ x--;
+ }
+ System.out.println("Factorial of the given number is 5 : "+fact);
+} 
+} 
