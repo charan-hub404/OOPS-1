b7c6a976ab0b6b14e7a6b2f551e13b42b406a49c
cherry <sreecharanlingampalli@gmail.com>
2025-08-02 11:29:25 +0530

    Add files via upload

---
+++ Test3.java
@@ -0,0 +1,9 @@
+package oops1;
+
+public class Test3 {
+    public static void main(String[] args) {
+        int i = 10;
+        boolean b = (i != 0); // true if i is not zero
+        System.out.println("b = " + b);
+    }
+}
